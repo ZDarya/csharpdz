576ae7b27a08c3c8eafc8af8bd7654b1c79eb9fc
Darya <d.v.zorina@yandex.ru>
2023-07-10 21:27:43 +0300

    Программа про вывод чисел

---
+++ Project4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Int32.Parse(Console.ReadLine());
+if (num < 0)
+{
+    num = num * -1;
+}
+for (int x = -num; x <= num; x++)
+{
+    Console.Write(x + " ");
+}
