5e8c7b8a00f0bbdefa98a5ac9cc0769aa6de4089
Darya <d.v.zorina@yandex.ru>
2023-07-10 22:36:36 +0300

    Решение задачи 3 из ДЗ

---
+++ DZProject3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным.
+Console.WriteLine("Введите число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int a = number1 % 2;
+if (a == 0)
+{
+    Console.Write("Число четное");
+}
+else
+{
+    Console.Write("Число нечетное");
+}
